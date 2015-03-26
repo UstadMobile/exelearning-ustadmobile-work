6f24d9fc25fa4d131536a6659105254b38ee3501
Mike Dawson <mike@toughra.com>
2015-03-26 19:04:53 +0400

    Update win-setup.py to include resumablejusclient

--- win-setup.py
+++ win-setup.py
@@ -77,8 +77,11 @@ dataFiles(["exe/jsui/templates",
 
 opts = {
  "py2exe": {
-   "packages": ["encodings", "nevow", "nevow.flat", "cProfile", "functools", "csv", "libxml2", "robotparser", "chardet", "lxml", "textstatistics", "feedparser", "BeautifulSoup"],
-   "packages": ["encodings", "nevow", "nevow.flat", "cProfile", "functools", "csv", "libxml2", "robotparser", "chardet", "lxml", "feedparser", "BeautifulSoup"],
+   "packages": ["encodings", "nevow", "nevow.flat", 
+                "cProfile", "functools", "csv", 
+                "libxml2", "robotparser", "chardet", 
+                "lxml", "textstatistics", "feedparser", 
+                "BeautifulSoup", "resumablejsclient"],
    "includes": ["PngImagePlugin", "JpegImagePlugin", "GifImagePlugin",
                 "IcoImagePlugin", "BmpImagePlugin"],
 
